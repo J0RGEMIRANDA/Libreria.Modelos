9f4c4fe36295593fdb7de2902b563b2e7f3bca31
Jorge <mirandaarcosandres@gmail.com>
2025-06-09 19:00:08 -0500

    Agregar archivos de proyecto.

---
+++ Libreria.MVC/Controllers/AutoresController.cs
@@ -0,0 +1,95 @@
+﻿using Libreria.Modelos;
+using Librerria.API.Consumer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libreria.MVC.Controllers
+{
+    public class AutoresController : Controller
+    {
+        // GET: AutoresController
+        public ActionResult Index()
+        {
+            var data = Crud<Autor>.GetAll();
+            return View(data);
+        }
+
+        // GET: AutoresController/Details/5
+        public ActionResult Details(int id)
+        {   
+            var data= Crud<Autor>.GetById(id);
+            return View(data);
+        }
+
+        // GET: AutoresController/Create
+        public ActionResult Create()
+        {   
+            return View();
+        }
+
+        // POST: AutoresController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Autor data)
+        {
+            try
+            { 
+                Crud<Autor>.Create(data);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex) 
+            {
+                ModelState.AddModelError("", $"Error creating author: {ex.Message}");
+                return View(data);
+            }
+        }
+
+        // GET: AutoresController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var data = Crud<Autor>.GetById(id);
+            return View(data);
+        }
+
+        // POST: AutoresController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Autor data)
+        {
+            try
+            {
+                Crud<Autor>.Update(id,data);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", $"Error editing author: {ex.Message}");
+                return View(data);
+            }
+        }
+
+        // GET: AutoresController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var data = Crud<Autor>.GetById(id);
+            return View(data);
+        }
+
+        // POST: AutoresController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Autor data)
+        {
+            try
+            {
+                Crud<Autor>.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", $"Error deleting author: {ex.Message}");
+                return View(data);
+            }
+        }
+    }
+}
---
+++ Libreria.MVC/Controllers/EditorialesController.cs
@@ -0,0 +1,95 @@
+﻿using Libreria.Modelos;
+using Librerria.API.Consumer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libreria.MVC.Controllers
+{
+    public class EditorialesController : Controller
+    {
+        // GET: EditorialesController
+        public ActionResult Index()
+        {   
+            var data = Crud<Editorial>.GetAll();
+            return View(data);
+        }
+
+        // GET: EditorialesController/Details/5
+        public ActionResult Details(int id)
+        {
+            var data = Crud<Editorial>.GetById(id);
+            return View(data);
+        }
+
+        // GET: EditorialesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EditorialesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Editorial data)
+        {
+            try
+            {
+                Crud<Editorial>.Create(data);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", $"Error creating editorial: {ex.Message}");
+                return View(data);
+            }
+        }
+
+        // GET: EditorialesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var data = Crud<Editorial>.GetById(id);
+            return View(data);
+        }
+
+        // POST: EditorialesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Editorial data)
+        {
+            try
+            {
+                Crud<Editorial>.Update(id, data);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", $"Error editing editorial: {ex.Message}");
+                return View(data);
+            }
+        }
+
+        // GET: EditorialesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var data = Crud<Editorial>.GetById(id);
+            return View(data);
+        }
+
+        // POST: EditorialesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Editorial data)
+        {
+            try
+            {
+                Crud<Editorial>.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", $"Error deleting editorial: {ex.Message}");
+                return View(data);
+            }
+        }
+    }
+}
